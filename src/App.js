4957760b5216b218734360643eed2208d88c4340
Eric Pan <epan@users.noreply.github.com>
2020-03-22 00:31:42 -0700

    add prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class LambdaDemo extends Component {
   constructor(props) {
@@ -12,7 +12,7 @@ class LambdaDemo extends Component {
     e.preventDefault()
 
     this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
+    fetch('/.netlify/functions/' + api)
       .then(response => response.json())
       .then(json => this.setState({ loading: false, msg: json.msg }))
   }
@@ -22,8 +22,12 @@ class LambdaDemo extends Component {
 
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        <button onClick={this.handleClick('hello')}>
+          {loading ? 'Loading...' : 'Call Lambda'}
+        </button>
+        <button onClick={this.handleClick('async-dadjoke')}>
+          {loading ? 'Loading...' : 'Call Async Lambda'}
+        </button>
         <br />
         <span>{msg}</span>
       </p>
